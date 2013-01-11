3bd704d31c2a49cf8f2843221b8a73cedf78082d
agnivalent <agnivalent@gmail.com>
2013-01-11 04:02:01 -0300

    getting data for first page

--- application.rb
+++ application.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'sinatra'
 require 'json'
 require 'tiny_tds'
@@ -9,31 +10,75 @@ db_client = TinyTds::Client.new(
 	:username => 'pxrptuser', 
 	:password => 'pxrptuser', 
 	:host => '82.138.55.29',
-	:database => 'PMDB')
+	:database => 'PMDB',
+  :encoding => 'CP1251')
 
 before /.*/ do
-  content_type :json  
+  content_type :json, 'charset' => 'utf-8' 
   headers("Access-Control-Allow-Origin" => "*")
 end
 
-get '/1/eps' do
-  result = []
-  query_result = db_client.execute("SELECT * FROM EPS WHERE objectid = #{options.eps_id};")
-  query_result.each{|row| result << row}
+get '/data/index' do
+  result = {}
+  # Полное наименование программы - eps_name
+  # Исполнительный директор программы - eps_exec_dir
+  # Директор программы - eps_dir
+  # Дата начала - start_date
+  # Дата окончания - end_date
+  # Отчёт подготовил - reporter
+  # Отчётный период -report_period
+  # Значение на графике - gauge_percent
+  # Прогнозная дата окончания программы - est_end_date
+  # Сроки - time_limits
+  # Стоимость - cost
+  # Риски - risks
+  eps_query_result = db_client.execute("SELECT sumcostpercentcomplete as gauge_percent,
+                                               name as eps_name,
+                                               convert(varchar,sumbaselinestartdate, 4) as start_date,
+                                               convert(varchar, sumbaselinefinishdate, 4) as end_date,
+                                               convert(varchar, finishdate, 4) as est_end_date,
+                                               (EPS.SumPlannedTotalCost - EPS.SumActualTotalCost) as cost
+
+                                        FROM EPS WHERE objectid = #{options.eps_id};")
+  result.merge!(eps_query_result.first)
+  eps_query_result.do
+
+  result['risks'] = "3 критичных риска"
+  result['time_limits'] = "Отсутствуют базовые показатели, т.к. базовый план не утвержден"
+  result['report_date'] = "15.05.12"
+  result['eps_exec_dir'] = "А. М. Слепцов"
+  result['eps_dir'] = "В. А. Благовещенский"
+
+  
+
   result.to_json
 end
 
-# get '/1/zero_project' do
-#   result = []
-#   query_result = db_client.execute("SELECT * FROM PROJECT WHERE objectid = #{options.eps_id};")
-#   query_result.each{|row| result << row}
-#   result.to_json
-# end
+get '/data/3' do
+  result = {}
+  # Освоенный объём (EV) - ev
+  # Факт. Затраты (АС) - ac
+  # Плановое освоение (PV) - pv
+  # Индекс выполнения сроков (SPI) - spi
+  # Отклонение по срокам (SV) - sv
+  # Индекс выполнения стоимости (CPI) - cpi
+  # Отклонение по стоимости (CV) - cv
+  # Длительность до заверш. (VACt) - vact
+  # Стоимость до завершения (VAC) - vac
+
+
+  eps_query_result = db_client.execute("SELECT sumcostpercentcomplete as gauge_percent FROM EPS WHERE objectid = #{options.eps_id};")
+  result = eps_query_result.first
+  eps_query_result.do
+
+  result.to_json
+end
 
 get '/db/:table' do
   result = []
   query_result = db_client.execute("SELECT * FROM #{params[:table]};")
   query_result.each{|row| result << row}
+  query_result.do
   result.to_json
 end
 
@@ -41,5 +86,6 @@ get '/tables' do
   result = []
   query_result = db_client.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
   query_result.each{|row| result << row}
+  query_result.do
   result.to_json
 end
---
+++ www/js/index.js
@@ -0,0 +1,231 @@
+//swipe-related
+var forward = function () {
+    $("div").delay(1).animate({
+                right: "-=560px",
+                opacity: "0.2"
+            }, 200, function() {
+                window.location = "2.html";
+            });
+
+};
+
+var backward = function() {
+    $("div").delay(1).animate({
+                right: "+=560px",
+                opacity: "0.2"
+            }, 200, function() {
+                window.location = "2.html";
+            });
+
+};
+
+$(document).addSwipeEvents().
+  bind('swiperight', function(evt, touch) {
+    // triggered for swipe events
+    backward();
+});
+
+
+
+//getting data from service
+var __data__ = null;
+
+var getData = function() {
+    if (__data__ != null) {
+        return __data__;
+    } else {
+        $.getJSON('http://94.127.69.63:8080/data/index', function(data) {
+        // $.getJSON('data/index', function(data) {
+            __data__ = data;
+            setDataToDom(data);
+            return data;
+        });
+    }
+};
+
+var setDataToDom = function(data) {
+    // $('#eps_name').html(data['name']);
+    // var d = new Date();
+    // d.setTime(Date.parse(data['sumbaselinestartdate']))
+    // $('#eps_sumbaselinestartdate').html($.datepicker.formatDate('dd.mm.yy', d));
+    // d.setTime(Date.parse(data['sumbaselinefinishdate']));
+    // $('#eps_sumbaselinefinishdate').html($.datepicker.formatDate('dd.mm.yy', d));
+
+    $('#eps_name').html(data['eps_name']);
+    $('#eps_exec_dir').html(data['eps_exec_dir']);
+    $('#eps_dir').html(data['eps_dir']);
+    $('#start_date').html(data['start_date']);
+    $('#end_date').html(data['end_date']);
+    $('#reporter').html(data['reporter']);
+    $('#report_date').html(data['report_date']);
+    $('#est_end_date').html(data['est_end_date']);
+    $('#time_limits').html(data['time_limits']);
+    $('#cost').html(data['cost']);
+    $('#risks').html(data['risks']);
+
+    plan_fact_gauge.series[0].points[0].update(parseFloat(data['gauge_percent']));
+
+};
+
+var plan_fact_gauge;
+
+$(document).ready(function() {
+    plan_fact_gauge = new Highcharts.Chart({
+
+        chart: {
+            renderTo: 'plan_fact_gauge',
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+
+        title: {
+            text: 'Шкала факт / план'
+        },
+
+plotOptions: {
+gauge: {
+                dial: {
+
+                    backgroundColor: '#f46c5e',
+                    borderColor: '#a5b4b9',
+                    borderWidth: 1,
+                    baseWidth: 13,
+                    topWidth: 1,
+                        baseLength: '20%', // of radius
+                        rearLength: '0%'
+                }
+            
+        },
+
+
+            series: {
+                dataLabels: {
+                    enabled: false,
+borderRadius:0,
+padding:10,
+x:-100,
+                    formatter: function() {
+                        return "Факт "+this.y +'%';
+                    }
+                }
+            }
+        },
+
+        pane: {
+            startAngle: -90,
+            endAngle: 90,
+            background: [{
+                backgroundColor: '#FFFFFF',
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: '#FFFFFF',
+                borderWidth: 0,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#FFFFFF',
+                borderWidth: 0,
+                outerRadius: '108%',
+                innerRadius: '9%'
+            }]
+        },
+
+
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 100,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+
+            tickPixelInterval: 30,
+            tickWidth: 1,
+            tickPosition: 'inside',
+            tickLength: 34,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                style: {fontSize: '15'},
+            distance: -87,
+
+
+         formatter: function() {
+                        return this.value +'%';
+                    }
+
+            },
+            title: {
+                text: ''
+            },
+            plotBands: [{
+                from: 0,
+                to: 20,
+                color: '#dde5f2', // green
+            innerRadius: '67%',
+ label: {
+                    style: {
+                        color: 'blue',
+                        fontWeight: 'bold'
+                    }
+                } 
+                
+            }, {
+                from: 20,
+                to: 30,
+                color: '#bacce4', // yellow
+                innerRadius: '67%'
+        }, {
+                from: 30,
+                to: 40,
+                color: '#bacce4', // red
+innerRadius: '67%'
+            } , {
+                from: 40,
+                to: 50,
+                color: '#96b3d5', // yellow
+innerRadius: '67%'
+            }, {
+                from: 50,
+                to: 60,
+                color: '#96b3d5', // yellow
+innerRadius: '67%'
+
+            }, {
+                from: 60,
+                to: 70,
+                color: '#5a8eca', // yellow
+innerRadius: '67%'
+            }, {
+                from: 70,
+                to: 80,
+                color: '#5a8eca', // yellow
+innerRadius: '67%'
+            }, {
+                from: 80,
+                to: 100,
+                color: '#376193', // yellow
+innerRadius: '67%'
+}]        
+        },
+
+        series: [{
+            name: 'Данные',
+            data: [0],
+            tooltip: {
+                valueSuffix: 'факт/план'
+            }
+        }]
+
+    });
+
+    getData();
+});
